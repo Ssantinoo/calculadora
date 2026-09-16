47a4cae9fe6c25db8ce167325e10350106c4fa9b
Santino <122167715+Ssantinoo@users.noreply.github.com>
2026-09-16 19:14:19 +0000

    Initial Commit - Basic Setup

---
+++ script.js
@@ -0,0 +1,38 @@
+let num1;
+let num2;
+let operator;
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        console.log("You cannot divide by zero")
+    }
+
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    } else {
+        console.log("Syntax Error");
+    }
+}
